b60e5ffe0e2d779dcc1669213bae5d57e902a9c1
RZhang <richardzhiyuanzhang@gmail.com>
2022-05-21 23:33:35 -0400

    Working on the general layout of the quiz, getting the buttons to switch between sections working, and the timer linked to the starting of the quiz. created an array of questions. Will work to implement them and create functions related to the quiz next.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,128 @@
 const highScoreBtn = document.querySelector("#highscore-button");
-const startButton = document.querySelector("#start");
-const backButton = document.querySelector(".back");
+const startBtn = document.querySelector("#start");
+const backBtn = document.querySelector("#back");
+const clearBtn = document.querySelector("#clear");
+const submitBtn = document.querySelector("#submit");
+const returnBtn = document.querySelector("#return");
 
 const timerEl = document.querySelector("#time-count");
 const titleQuestionEl = document.querySelector("#title");
-const optionsAnswersEl = document.querySelectorAll("options");
+const optionsAnswersEl = document.querySelector("options");
 const responseEl = document.querySelector("#response");
 
 const introSection = document.querySelector(".intro");
 const quizSection = document.querySelector(".quiz");
 const scoresSection = document.querySelector(".highscores");
+const resultsSection = document.querySelector(".quiz-results");
 
 introSection.hidden = false;
 quizSection.hidden = true;
 scoresSection.hidden = true;
+resultsSection.hidden = true;
 
 let score = 0;
-let timerCount = 10;
+let questionsCount = 0;
 let allQuestionsAnswered = false;
 
+const questions = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answer: "<script>",
+        options: ["<js>", "<script>", "<javascript>", "<scripting>"]
+    },
+
+    {
+        question: "Where is the correct place to insert a JavaScript?",
+        answer: "The <body> section",
+        options: ["Both the <head> section and the <body section are correct", 
+        "The <body> section", "The <head> section"]
+    },
+
+    {
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheets",
+        options: ["Colorful Style Sheets", "Computer Style Sheets",
+        "Creative Style Sheets", "Cascading Style Sheets"]
+    },
+
+    {
+        question: "To make a comment in HTML you use:",
+        answer: "<!--  -->",
+        options: ["//", "/*", "#", "<!--  -->"]
+    },
+
+    {
+        question: "What is a variable?",
+        answer: "Store values so we can use them later and change them from the code.",
+        options: ["Store values so we can use them later and change them from the code", 
+        "Store values so we can use them but cannot change them",
+        "Store values so we can use them once", 
+        "Store values in containers so we can't use them later"]
+    }
+];
+
+const checkQuizOver = () => {
+    if (questionsCount === questions.length) {
+        allQuestionsAnswered = true;
+    }
+}
 
 const startTimer = () => {
+    let timerCount = 10;
 
-    const timer = setInterval(function() {
+    const timer = setInterval(function () {
 
         timerCount--;
         timerEl.textContent = timerCount;
-        
-        if (timerCount >= 0) {          
-          if (allQuestionsAnswered && timerCount > 0) {
+
+        if (timerCount >= 0) {
+            if (allQuestionsAnswered && timerCount > 0) {
+                clearInterval(timer);
+                displayQuizResults();
+            }
+        }
+
+        if (timerCount === 0) {
             clearInterval(timer);
-            displayScore();
-          }
+            displayQuizResults();
         }
 
-        if (timerCount === 0) {    
-          clearInterval(timer);
-          displayScore();
-        }
-
-      }, 1000);
+    }, 100);
 }
 
 const startQuiz = () => {
+    allQuestionsAnswered = false;
     introSection.hidden = true;
     quizSection.hidden = false;
+    resultsSection.hidden = true;
     highScoreBtn.disabled = true;
     startTimer();
+}
+
+const displayQuizResults = () => {
+    resultsSection.hidden = false;
+    introSection.hidden = true;
+    quizSection.hidden = true;
+    scoresSection.hidden = true;
 }
 
 const displayScores = () => {
     introSection.hidden = true;
     quizSection.hidden = true;
     scoresSection.hidden = false;
+    resultsSection.hidden = true;
+    highScoreBtn.disabled = false;
 }
 
 const displayMain = () => {
     introSection.hidden = false;
     quizSection.hidden = true;
     scoresSection.hidden = true;
+    resultsSection.hidden = true;
+    highScoreBtn.disabled = false;
 }
 
-startButton.addEventListener("click", startQuiz);
+startBtn.addEventListener("click", startQuiz);
 highScoreBtn.addEventListener("click", displayScores);
-backButton.addEventListener("click", displayMain);
+backBtn.addEventListener("click", displayMain);
+returnBtn.addEventListener("click", displayMain);
